b6dd0ff8b4447f4f52778a1f01f4ba4bc2cde7f0
ashleyalimbe03 <ashleyalimbe16@gmail.com>
2025-01-30 02:31:47 +0000

    test

--- conjoint..py
+++ conjoint..py
@@ -2,12 +2,9 @@ def som_dij(n):
     return sum(int(digit) for digit in str(n))
 
 def conjoin(s_1, s_2):
-    while s_1 != s_2
-        if s_1 < s_2
-        s_1 = s_1+som_dij(s_1)
-        else 
-        s_2 = s_2+som_dij(s_2)
-return s_2
-
-
-
+    while s_1 != s_2 :
+        if s_1 < s_2 :
+         s_1 = s_1+som_dij(s_1)
+        else :
+         s_2 = s_2+som_dij(s_2)
+    return s_2
